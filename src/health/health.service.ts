d8def3c1068081c7a945c7f37079a74c92e593f8
bielb1b2 <gabrielvt.souza@hotmail.com>
2025-08-08 00:55:06 -0300

    feat: liveness, readiness, startup

--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -3,10 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class HealthService {
   checkHealth(): string {
+    console.log('Health Executado!');
     return 'OK';
   }
 
   checkReady(): string {
+    console.log('Check Executado!');
     return 'OK!';
   }
 }
